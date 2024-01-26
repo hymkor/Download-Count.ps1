202c27ca59dd03729e2b51457f076d0a9c4533e3
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-26 20:15:46 +0900

    OWNER and REPOSITORY can be given with the first argument

--- Download-Count.ps1
+++ Download-Count.ps1
@@ -3,7 +3,7 @@ function Get-Repo {
     $branch = (git remote show -n | Select-Object -First 1)
     if ( $branch ) {
         foreach ( $line in (git remote show -n $branch) ){
-            if ( $line -match "Push +URL: \w+@github.com:([\w-]+/[\w-]+).git" ){
+            if ( $line -match "Push +URL: \w+@github.com:([\w-\.]+/[\w-\.]+).git" ){
                 return $matches[1]
             }
         }
@@ -11,11 +11,21 @@ function Get-Repo {
     return $null
 }
 
-$repo = (Get-Repo)
-if ( -not $repo ){
-    Write-Host "git information not found"
-    exit 1
+if ( $args[0] ){
+    $repo = $args[0]
+    if ( -not $repo -match "^[\w-\.]+/[\w-\.]+$" ){
+        Write-Host "Usage: Download-Count.ps1 [OWNERNAME/REPOS]"
+        exit 1
+    }
+
+} else {
+    $repo = (Get-Repo)
+    if ( -not $repo ){
+        Write-Host "git information not found"
+        exit 1
+    }
 }
+
 $url = "https://api.github.com/repos/$repo/releases"
 
 Invoke-WebRequest -URI $url | ConvertFrom-Json | ForEach-Object {
